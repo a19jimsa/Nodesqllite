e58c921a27540f67fee21328fbfb60e6c9c42aa0
a19jimsa <a19jimsa@student.his.se>
2021-11-29 11:16:37 +0100

    Added node and express

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.use('/', express.static('src'))
+app.use('/components', express.static('components'))
+
+const usersRoute = require("./usersRoute")
+app.use("/users", usersRoute)
+
+const climatecodesRoute = require("./climatecodesRoute")
+app.use("/climatecodes", climatecodesRoute)
+
+const commentsRoute = require("./commentsRoute")
+app.use("/comments", commentsRoute);
+
+const forecastRoute = require("./forecastRoute")
+app.use("/forecast", forecastRoute);
+
+//Hämtar ut startup.html när localhost:3000 körs.
+app.get('/', function(req, res){
+    res.sendFile('src/startup.html',{root: '.'})
+    console.log("Start");
+})
+
+app.listen(3000, function (){
+    console.log("Server started")
+})
